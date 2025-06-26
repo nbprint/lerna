340db43506a946728abbf46e3243429f60f35a73
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-06-26 18:09:24 -0400

    Reinitialize properly this time

--- hydra_plugins/lerna/searchpath.py
+++ hydra_plugins/lerna/searchpath.py
@@ -12,9 +12,15 @@ _log = getLogger("lerna")
 _discovered_plugins: EntryPoints = entry_points(group="hydra.lernaplugins")
 for entry_point in _discovered_plugins:
     try:
-        globals()[entry_point.name] = import_module(entry_point.value)
+        mod = import_module(entry_point.value)
     except ImportError as e:
         _log.warning(f"Failed to import entry point {entry_point.name} from {entry_point.value}: {e}")
+        continue
+    for attr in dir(mod):
+        thing = getattr(mod, attr)
+        if isinstance(thing, type) and issubclass(thing, SearchPathPlugin):
+            _log.info(f"Discovered search path plugin: {thing.__name__}")
+            globals()[thing.__name__] = thing
     # search_path.append(provider=entry_point.name, path=entry_point.value)
 
 
