5a5fc295e9bb3eb0bcdfcb989029f7581586d80d
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-02-26 23:49:22 -0500

    Bump version: 2.0.0 → 2.0.1
    Signed-off-by: Tim Paine <3105306+timkpaine@users.noreply.github.com>

--- lerna/__init__.py
+++ lerna/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
 # Source of truth for Lerna's version (Hydra-compatible API)
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 # Callback support from Rust
 from lerna import lerna as _rust, utils
